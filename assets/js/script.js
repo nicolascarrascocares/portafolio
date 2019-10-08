ddd18282217d1c3a80fa4d56925fb67a7a84ad24
Nicolas <nicolascarrasco15@gmail.com>
2019-10-08 16:24:13 -0300

    Indentacion del codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+ 
+    $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+     
+        if (this.hash !== "") {
+      
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-    
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+
+               
+                window.location.hash = hash;
+            });
+        }
+    });
+
 
     $('[data-toggle="tooltip"]').tooltip()
 });
@@ -41,5 +38,5 @@ $(document).ready(function(){
 
 
 
-  
+
 
