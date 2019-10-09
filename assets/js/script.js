836244416ec4be8dcece6000111d1a1cd77cde91
Nicolas <nicolascarrasco15@gmail.com>
2019-10-08 21:59:54 -0300

    Commit con cambios en el body y archivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,21 @@
 $(function(){
- 
+
     $("a").on('click', function(event) {
-
-     
         if (this.hash !== "") {
-      
             event.preventDefault();
-
             var hash = this.hash;
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 1000, function(){
-
-               
                 window.location.hash = hash;
             });
         }
     });
-
-
-    $('[data-toggle="tooltip"]').tooltip()
+    $("h2").click(function(){
+        $(this).css("color","red")
+    });
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 
